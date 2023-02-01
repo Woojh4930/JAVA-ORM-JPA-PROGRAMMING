63dbdef1648f639e75524ed2cee8e3d265e1c37e
juhwang <juhong4930@naver.com>
2023-02-01 17:56:59 +0900

    feat: ch04.엔티티 매핑 초기 기능 구현

---
+++ ch04/src/main/java/jpabook/practice/Member.java
@@ -0,0 +1,34 @@
+package jpabook.practice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Getter
+@Setter
+@Table(name = "MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+}
---
+++ ch04/src/main/java/jpabook/practice/RoleType.java
@@ -0,0 +1,5 @@
+package jpabook.practice;
+
+public enum RoleType {
+    ADMIN,USER
+}
